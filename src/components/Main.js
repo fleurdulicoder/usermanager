078f2105c04a8c012e324bd9aef5d14c8bbea866
[ellam] <[ella.musina@orionhealth.com]>
2017-08-31 11:15:02 -0700

    Title updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,7 +15,7 @@ class Main extends Component {
   defaultScreen() {
     return (
       <Card className="user-card appear">
-        <CardHeader title="Welcome" />
+        <CardHeader title="WELCOME" />
         <CardText>Load users from the left hand-side panel</CardText>
       </Card>
     );
@@ -24,7 +24,7 @@ class Main extends Component {
   render() {
     return (
       <div className="app">
-        <AppBar title="User Manager" className="appbar" />
+        <AppBar title="USER MANAGER" className="appbar" />
         <UserList />
         <div className="main">
           <Route exact path="/" component={this.defaultScreen} />
